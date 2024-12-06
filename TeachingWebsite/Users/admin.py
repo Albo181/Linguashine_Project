31d7c53167744d115c075db0b8bcdea81d1460e9
Alex L <purple_ronnie3@hotmail.com>
2024-12-07 00:41:32 +0100

    Update CustomUserAdmin to implement multi-step user creation

--- TeachingWebsite/Users/admin.py
+++ TeachingWebsite/Users/admin.py
@@ -1,28 +1,74 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import CustomUser
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
+class CustomUserDetailsForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'telephone', 'bio', 'user_type', 'forum_access')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.required = False
+
 class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserDetailsForm
     model = CustomUser
     list_display = ('username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff')
     list_filter = ('is_staff', 'is_active', 'user_type')
     
-    # Customize fieldsets for user creation and editing
+    # Customize fieldsets for user editing
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'telephone', 'bio', 'profile_picture')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Type', {'fields': ('user_type', 'forum_access')}),
     )
+
+    # First step of user creation - basic info only
     add_fieldsets = (
-        (None, {
+        ('Basic Information', {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'user_type', 'is_staff', 'is_active')}
-        ),
+            'fields': ('username', 'email', 'password1', 'password2'),
+            'description': 'Please enter the basic user information. Additional details can be added in the next step.'
+        }),
     )
     
     search_fields = ('email', 'username', 'first_name', 'last_name')
     ordering = ('email',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during user creation
+        """
+        defaults = {}
+        if obj is None:
+            defaults['form'] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
+
+    def response_add(self, request, obj, post_url_continue=None):
+        """
+        Determine the HttpResponse for the add_view stage.
+        """
+        # We need to distinguish between different stages:
+        if '_addanother' not in request.POST and '_continue' not in request.POST:
+            request.POST._mutable = True
+            request.POST['_continue'] = 1
+            request.POST._mutable = False
+        return super().response_add(request, obj, post_url_continue)
 
 admin.site.register(CustomUser, CustomUserAdmin)
