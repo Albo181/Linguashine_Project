08e19e1fbfe0df59d6b53f422a5e6290cad57184
Alex L <purple_ronnie3@hotmail.com>
2025-12-08 17:27:01 +0100

    Remove unused meshirt import from SobreMi component

--- TeachingFront/teaching_front/src/components/SobreMi.jsx
+++ TeachingFront/teaching_front/src/components/SobreMi.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
-import meshirt from '../images/meshirt.jpg';
 import me_maybe from '../images/me_maybe.png';
 
 const SobreMi = () => {
